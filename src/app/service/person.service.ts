50ff93746b9a287a09a74350726567096d32e9dc
marinanc <marinancarrizo@gmail.com>
2023-03-02 23:52:33 -0300

    Add getPerson() to fetch person with id 1

--- src/app/service/person.service.ts
+++ src/app/service/person.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { person } from '../model/person.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
 
-  constructor() { }
+  URL = 'http://localhost:8080/persons/';
+
+  constructor(private http: HttpClient) { }
+
+  public getPerson(): Observable<person>{
+    return this.http.get<person>(this.URL + 'get/profile');
+  }
 }
