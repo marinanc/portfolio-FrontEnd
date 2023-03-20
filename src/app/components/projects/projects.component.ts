4f81647007cfbb1c0be9495222c3e6f89592673a
marinanc <marinancarrizo@gmail.com>
2023-03-20 03:16:19 -0300

    Update projects.component.ts

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/model/project';
+import { ProjectService } from 'src/app/service/project.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  project: Project[] = [];
+
+  constructor(private projectService: ProjectService, private tokenService: TokenService) { }
+
+  isLogged = false;
 
   ngOnInit(): void {
+    this.loadProject();
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+
+  loadProject(): void {
+    this.projectService.list().subscribe( data => {this.project = data} );
+  }
+
+  delete(id?: number): void {
+    if(id != undefined) {
+      this.projectService.delete(id).subscribe( data => {
+        this.loadProject();
+      }, err => {
+        alert("No se pudo eliminar el proyecto");
+      })
+    }
   }
 
 }
