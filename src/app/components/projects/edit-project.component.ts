d98feaaaf11fa738c8612ebec4f9a57e3d42e7bf
marinanc <marinancarrizo@gmail.com>
2023-03-20 03:26:21 -0300

    Update edit and new Project

--- src/app/components/projects/edit-project.component.ts
+++ src/app/components/projects/edit-project.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Project } from 'src/app/model/project';
+import { ProjectService } from 'src/app/service/project.service';
 
 @Component({
   selector: 'app-edit-project',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProjectComponent implements OnInit {
 
-  constructor() { }
+  project: Project = null;
+
+  constructor(private projectService: ProjectService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.projectService.detail(id).subscribe( data => {
+      this.project = data;
+    }, err => {
+      alert("Error al modificar el proyecto");
+      this.router.navigate(['']);
+    })
+  }
+
+  onUpdate(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.projectService.update(id, this.project).subscribe( data => {
+      this.router.navigate(['']);
+    }, err => {
+      alert("Error al modificar el proyecto");
+      this.router.navigate(['']);
+    })
   }
 
 }
--- src/app/components/projects/new-project.component.ts
+++ src/app/components/projects/new-project.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Project } from 'src/app/model/project';
+import { ProjectService } from 'src/app/service/project.service';
 
 @Component({
   selector: 'app-new-project',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewProjectComponent implements OnInit {
 
-  constructor() { }
+  name: string = '';
+  description: string = '';
+  link: string = '';
+
+  constructor(private projectService: ProjectService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onCreate(): void {
+    const project = new Project(this.name, this.description, this.link);
+    this.projectService.save(project).subscribe( data => {
+      alert("Proyecto añadido");
+      this.router.navigate(['']);
+    }, err => {
+      alert("No se pudo añadir el nuevo proyecto");
+      this.router.navigate(['']);
+    })
+  }
 }
